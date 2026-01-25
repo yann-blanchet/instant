231673d41a9471b67b586130151c608af25b4363
Yann blanchet <yanblanchet@gmail.com>
2026-01-25 19:01:56 +0100

    feat: add browser-image-compression to optimize dependencies and build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,12 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
+  optimizeDeps: {
+    include: ['browser-image-compression'],
+  },
+  build: {
+    commonjsOptions: {
+      include: [/browser-image-compression/, /node_modules/],
+    },
+  },
 });
